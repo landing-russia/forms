0219730cf988291383e195feba79015dc3296ce8
landing-russia <landing-russia@yandex.ru>
2022-04-03 01:28:12 +0300

    first commit 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
 import preprocess from "svelte-preprocess";
+import vercel from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: [".svelte", ...mdsvexConfig.extensions],
@@ -8,6 +9,7 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
+    adapter: vercel()
   },
 
   preprocess: [
